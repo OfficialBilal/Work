93b78f24d1f5fee3a1c392c7030eaec3fe3ce214
OfficialBilal <chbilal.official@gmail.com>
2025-03-12 16:41:04 +0500

    Switch Case

---
+++ If-Else.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class If_Else
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello how are you?");
+        } 
+    }
+}
---
+++ NestedIfElse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+    internal class NestedIfElse
+    {
+        static void Main(string[] args)
+        {
+            string email, password;
+            {
+                Console.Write("Enter your Email: ");
+                email = Console.ReadLine().ToLower();
+
+                if (email == "bilal@gmail.com")
+                {
+                    {
+                        Console.Write("Enter your Password: ");
+                        password = Console.ReadLine();
+                        if (password == "Bilal123")
+                        {
+                            Console.WriteLine("Login Successful...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Password, try again.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email, try again.");
+                }
+            }
+        }
+    }
