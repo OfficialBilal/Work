7a9e98f3752e27904eafbab4755b23b0d185e0ff
OfficialBilal <chbilal.official@gmail.com>
2025-03-14 00:42:28 +0500

    Nested If Else

--- NestedIfElse.cs
+++ NestedIfElse.cs
@@ -7,41 +7,43 @@ namespace Work
         static void Main(string[] args)
         {
             string email, password;
-            {
-                string choice = string.Empty;
-                do
-                { 
-                   Console.Write("Enter your Email: ");
-                   email = Console.ReadLine().ToLower();
+            string choice;
 
-                    if (email == "abc")
+            do
+            {
+                Console.Write("Enter your Email: ");
+                email = Console.ReadLine();
+
+                if (email == "abc")
+                {
+                    Console.Write("Enter your Password: ");
+                    password = Console.ReadLine();
+
+                    if (password == "123")
                     {
-                        Console.Write("Enter your Password: ");
-                        password = Console.ReadLine();
-                        {
-                            if (password == "123")
-                            {
-                                Console.WriteLine("Login Successful...");
-                                do
-                                {
-                                    Console.WriteLine("Do you want to try again Yes / No");
-                                    choice = Console.ReadLine().ToLower();
-                                    if (choice != "yes" && choice != "no")
-                                    {
-                                        Console.WriteLine("Invalid Choice");
-                                    }
-                                } while (choice != "yes" && choice != "no");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You have Entered Wrong Password");
-                            }
-                        }
+                        Console.WriteLine("Login Successfull...");
                     }
-                    else { Console.WriteLine("You have Entered Wrong Email"); }
-                      
-                } while (choice == "yes");
-            }
+                    else
+                    {
+                        Console.WriteLine("Invalid Password");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email");
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to try again? Yes or No");
+                    choice = Console.ReadLine()?.ToLower();
+
+                    if (choice != "yes" && choice != "no")
+                    {
+                        Console.WriteLine("Invalid Choice, Please Selecct Yes or No");
+                    }
+                } while (choice != "yes" && choice != "no");
+            } while (choice == "yes");
         }
     }
 }
+          
