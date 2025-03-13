13f34a92908e574e73512602529b6686424ec644
OfficialBilal <chbilal.official@gmail.com>
2025-03-13 22:34:37 +0500

    While Loop

---
+++ WhileLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while(i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine("Loop Ended");
+        }
+    }
+}
