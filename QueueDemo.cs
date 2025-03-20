46ada9db31188c04bf322f207d56698909ddf8f5
OfficialBilal <chbilal.official@gmail.com>
2025-03-21 00:43:51 +0500

    Queue

---
+++ QueueDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class QueueDemo
+    {
+        void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue(32);
+            q.Enqueue(32.3);
+            q.Enqueue("Bilal");
+            q.Enqueue("C# Tutorial");
+            q.Enqueue('B');
+
+            Console.WriteLine(q.Count);
+
+            q.Dequeue(); //FIFO
+
+            Console.WriteLine(q.Peek());
+
+            while (q.Count > 0)
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+            
+            Console.WriteLine(q.Contains("Bilal"));
+            Console.WriteLine(q.Count);
+
+            foreach (object item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (object item in q)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
